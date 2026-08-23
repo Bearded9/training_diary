87d0169f1aca5ff155abb99a0e970b38a06a02bd
Alexandr Bryukhanov <alexbruyhanov@yandex.ru>
2026-08-24 02:15:04 +0700

    fix: исправлена опечатка, добавлен файл с текстовыми функциями, вызов теста сделал через функции

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import sys
 from datetime import datetime
+import messages
 import os
 
 # функция для проверки ввода
@@ -19,13 +20,13 @@ file_name_training_program = f"training_{timestamp}.txt"
 with open("programs.json", "r", encoding="utf-8") as f:
     programs = json.load(f)
 
-choosing_training_program = safe_int_input("Выберите готовую программу тренировок или создайте свою(введите '1' для создания своей программы или '2' для выбора готовой) ")
+choosing_training_program = safe_int_input(messages.text_ready_training_program())
 while choosing_training_program != 1 and choosing_training_program != 2:
     print("Введите '1' или '2'")
-    choosing_training_program = safe_int_input("Выберите гоовую программу тренировок или создайте свою(введите '1' для создания своей программы или '2' для выбора готовой) ")
+    choosing_training_program = safe_int_input(messages.text_ready_training_program())
 
 if choosing_training_program == 1:
-    list_of_exercises = input("Введите название упражнения или 'нет' для выхода: ")
+    list_of_exercises = input(messages.text_name_exercise())
     with open (file_name_training_program, "a", encoding="utf-8") as file:
         file.write("--- Своя программа! --- \n")
     while list_of_exercises != "нет":
@@ -38,7 +39,7 @@ if choosing_training_program == 1:
             file.write(f"{list_of_exercises} - {weight}кг, {number_of_approaches} на {number_of_repetitions} \n")
 
         print("Упражение добавлено")
-        list_of_exercises = input("Введите название упражнения или 'нет' для выхода: ")
+        list_of_exercises = input(messages.text_name_exercise())
 
     print("Тренировка сохранена")
 
@@ -47,14 +48,14 @@ if choosing_training_program == 2:
     for key, program in programs.items(): # чтение готовых программ тренировок
         print(f"{key}. {program['name']} ({', '.join(program['exercises'])})")
 
-    choice = input("Выберите номер программы (или 'нет' для выхода): ")
+    choice = input(messages.text_number_program())
 
     while choice not in programs:
         if choice == "нет":
             print("До встречи!")
             sys.exit(0)
         print("Неправильный номер программы. Попробуйте еще раз.")
-        choice = input("Выберите номер программы (или 'нет' для выхода): ")
+        choice = input(messages.text_number_program())
 
     selected_program = programs[choice]
     print(f"\nВы выбрали программу: {selected_program['name']}")
